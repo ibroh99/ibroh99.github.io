b6cb52e5b79fe2ab815079075f3912df1cd6065e
ibroh99 <manusiagaptek99@gmail.com>
2021-06-03 03:36:23 +0700

    responsive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,80 @@
+/*------------------- navigation menu -------------------*/
+(() => {
+    const hamburgerBtn = document.querySelector('.hamburger-btn'),
+        navMenu = document.querySelector('.nav-menu'),
+        closeNavBtn = navMenu.querySelector('.close-nav-menu');
+
+    hamburgerBtn.addEventListener('click', showNavMenu);
+    closeNavBtn.addEventListener('click', hideNavMenu);
+
+    function showNavMenu() {
+        navMenu.classList.add('open');
+        bodyScrollingToggle();
+    }
+
+    function hideNavMenu() {
+        navMenu.classList.remove('open');
+        fadeOutEffect();
+        bodyScrollingToggle();
+    }
+
+    function fadeOutEffect() {
+        document.querySelector('.fade-out-effect').classList.add('active');
+        setTimeout(() => {
+            document
+                .querySelector('.fade-out-effect')
+                .classList.remove('active');
+        }, 300);
+    }
+
+    document.addEventListener('click', (e) => {
+        if (e.target.classList.contains('link-item')) {
+            if (e.target.hash !== '') {
+                e.preventDefault();
+                const hash = e.target.hash;
+
+                document.querySelector('.section.active').classList.add('hide');
+                document
+                    .querySelector('.section.active')
+                    .classList.remove('active');
+
+                document.querySelector(hash).classList.add('active');
+                document.querySelector(hash).classList.remove('hide');
+
+                navMenu
+                    .querySelector('.active')
+                    .classList.add('outer-shadow', 'hover-in-shadow');
+                navMenu
+                    .querySelector('.active')
+                    .classList.remove('active', 'inner-shadow');
+
+                if (navMenu.classList.contains('open')) {
+                    e.target.classList.add('active', 'inner-shadow');
+                    e.target.classList.remove(
+                        'outer-shadow',
+                        'hover-in-shadow'
+                    );
+
+                    hideNavMenu();
+                } else {
+                    let navItems = navMenu.querySelectorAll('.link-item');
+                    navItems.forEach((item) => {
+                        if (hash === item.hash) {
+                            item.classList.add('active', 'inner-shadow');
+                            item.classList.remove(
+                                'outer-shadow',
+                                'hover-in-shadow'
+                            );
+                        }
+                    });
+                    fadeOutEffect();
+                }
+                window.location.hash = hash;
+            }
+        }
+    });
+})();
+
 /*------------------- about section tabs -------------------*/
 (() => {
     const aboutSection = document.querySelector('.about-section'),
@@ -167,4 +244,14 @@ function bodyScrollingToggle() {
             popup.scrollTo(0, projectDetailsContainer.offsetTop);
         }
     }
+})();
+
+/*------------------- hide all sections except active -------------------*/
+(() => {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach((section) => {
+        if (!section.classList.contains('active')) {
+            section.classList.add('hide');
+        }
+    });
 })();
